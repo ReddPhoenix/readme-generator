2d2d0365e6ed846952f09b1036208d494c8faf8e
ReddPhoenix <wsguerrero@gmail.com>
2020-09-21 21:32:53 -0600

    initial files

---
+++ index.js
@@ -0,0 +1,30 @@
+// array of questions for user
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
+
+
+// Generated README will require the following:
+// *Title
+// *Description
+// *Table of Contents
+// *Installation
+// *Usage
+// *License
+// *Contributing
+// *Tests
+// *Questions
+
+// *Generated README will include 1 badge spcific to the repository
