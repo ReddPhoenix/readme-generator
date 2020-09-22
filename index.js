ec3e31f68d609b5c5a48156f521b15d4f3fc791a
ReddPhoenix <wsguerrero@gmail.com>
2020-09-21 23:05:10 -0600

    user questions created

--- index.js
+++ index.js
@@ -1,11 +1,61 @@
 // array of questions for user
-const questions = [
-
+const questions = [{
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Could you describe your project?"
+    },
+    // {
+    //     table of contents???
+    // },
+    {
+        type: "input",
+        name: "installation",
+        message: "What are the installation instructions for the project?"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Describe how your project is used."
+    },
+    // {
+    //     // Should license option be provided or have user select license?
+    //     type: "input",
+    //     name: "license",
+    //     message: " "
+    // },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Enter any additional contributors to your project."
+        // Full names or Github usernames? Able to list multiple contributors?
+    },
+    {
+        // What type of tests?
+        type: "input",
+        name: "test",
+        message: "Tests?"
+    },
+    {
+        // Contact via Github or email? Should option be presented to user?
+        type: "input",
+        name: "question",
+        message: "In case of questions, what is your contact information?"
+    }
+    // How to ask user to provide badges?
+    // How to ask user to provide link to project on Github?
+    // How to ask user to provide link to live app?
+    // How to ask user to provide links to screenshots/GIFs/videos?
+    // Is "question" able to link to Github username?
+    
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
 function init() {
